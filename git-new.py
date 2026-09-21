609456470151a81817f2975ebc63037a759f1ddf
Amrutha V <amrutha2124@gmail.com>
2026-09-21 17:05:39 +0530

    My first commit

---
+++ git-new.py
@@ -0,0 +1,17 @@
+x = 10
+y = 20  
+print(x+y)
+
+name = "Alex"  
+print("Hello", (name))
+
+
+a = 5
+
+b = 10
+a, b = b, a
+print("a:", a)  
+print("b:", b)  
+
+age = input("Enter your age: ")
+print("You are",age, "years old")
